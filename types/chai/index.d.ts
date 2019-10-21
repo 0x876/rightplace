d1748a2e901cc5920821dcab956005e68cafe087
buidlman <mr.tolstoy@protonmail.ch>
2019-10-21 22:45:00 +0300

    fix typescript

--- types/chai/index.d.ts
+++
@@ -1,7 +0,0 @@
-// tslint:disable:no-namespace member-ordering ban-types unified-signatures variable-name callable-types
-declare namespace Chai {
-    // tslint:disable-next-line:interface-name
-    interface Assertion extends LanguageChains, NumericComparison, TypeComparison {
-        bignumber: Assertion;
-    }
-}
